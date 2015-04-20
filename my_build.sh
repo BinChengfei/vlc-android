65b440bf1a9dc378f1d056fc04287ed29c64ce4f
BinChengfei <BinChengfei@gmail.com>
2015-04-20 09:44:23 +0800

    add my_build.sh to help building.

---
+++ my_build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+export ANDROID_SDK=/home/aFei/work/google/android-sdk-linux
+export ANDROID_NDK=/home/aFei/work/google/android-ndk-r10d
+export PATH=$PATH:$ANDROID_SDK/platform-tools:$ANDROID_SDK/tools
+
+[ $# -eq 1 -a "$1" = "clean" ] && {
+	echo "clean..."
+	rm vlc/build-android-arm-linux-androideabi/ -rf
+	rm vlc/contrib/*anrdroid* -rf
+
+	exit 0
+}
+
+sh compile.sh -a armeabi-v7a
+
+#sh compile.sh -a armeabi-v7a  relase
